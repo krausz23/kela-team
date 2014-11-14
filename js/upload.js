8071db42f336670eb49d2aab9ee48a9e57f5f746
andris11m <andras.strango@yahoo.com>
2014-11-13 18:22:06 -0800

    Beginning of music upload form backend
    A zene feltöltő formhoz elkezdtem JavaScriptben a validálást, és
    kezdetlegesen megvan a feltöltés, még kell rajta dolgozni.

--- js/upload.js
+++ js/upload.js
@@ -1,19 +1,97 @@
 $(document).ready(function(){
 	$("#musicUploadDiv").css("display","none");
 	$("#movieUploadDiv").css("display","none");
-  	$("#categorySelect").change(function(){
+ $("#categorySelect").change(function(){
 
-  		if ($('option:selected', this).text() == 'Music') {
-  			$("#musicUploadDiv").css("display","block");
-  			$("#movieUploadDiv").css("display","none");
-  		}
-  		else if($('option:selected', this).text() == 'Movie'){
-  			$("#musicUploadDiv").css("display","none");
-  			$("#movieUploadDiv").css("display","block");
-  		}
-	});
+  if ($('option:selected', this).text() == 'Music') {
+   $("#musicUploadDiv").css("display","block");
+   $("#movieUploadDiv").css("display","none");
+ }
+ else if($('option:selected', this).text() == 'Movie'){
+   $("#musicUploadDiv").css("display","none");
+   $("#movieUploadDiv").css("display","block");
+ }
 });
 
+var serverURL = "http://localhost/kela-team/kela-team/";
 
+  $("#submitButton").click(function(){
+    var torrentName = $(".torrentName").val();
+    if (old_password == "") {
+      $( ".torrentNameError" ).text( "* This field is required." );
+              //return false;
+    }
+    else {
+      $.ajax({
+        type : 'POST',          
+        url : serverURL + "musicupload.php", // Servlet URL          
+        data:{
+          user_id: 1,
+          torrent_neve: torrentName
+        },
+        success : function(response) {     
+          alert(response);
+        },
+        error : function(xhr, type) {
+          alert('Server error occurred');
+        }
+        });   
+    }
+  });
+  
+});
 
+/*-------------------------------------------EZ CSAK SEGÍTSÉG ------------------------------------------*/
+/*$("body").on("tap", "#submitEditProfileButton", function(){
+  var old_password = $("#oldPassword").val();
+  var new_password = $("#newPassword").val();
+  var confirm_password = $("#confirmPassword").val();
+  if (old_password == "") {
+    alert("Please enter the old password!");
+    $("#oldPassword").focus();
+            //return false;
+          } else if (new_password == "") {
+            alert("Please enter the new password!");
+            $("#newPassword").focus();
+            //return false;
+          } else if (confirm_password == "") {
+            alert("Please confirm the new password!");
+            $("#confirmPassword").focus();
+            //return false;
+          } else if (old_password.length < 6) {
+            alert("Your old password is at least 6 characters long.");
+            $("#oldPassword").focus();
+            //return false;
+          } else if (new_password.length < 6) {
+            alert("Password must be at least 6 characters!");
+            $("#newPassword").focus();
+            //return false;
+          } else if (new_password.localeCompare(confirm_password) != 0) {
+            alert("Your new password and confirmation password do not match!");
+            $("#confirmPassword").focus();
+            //return false;
+          }
+          else {
+            $.ajax({
+              type : 'POST',          
+                url : serverURL + "changepassword", // Servlet URL          
+                data:{
+                  user_id: user_id,
+                  old_password: old_password,
+                  new_password: new_password
+                },
+                success : function(data) {        
+                  if(data == 1){                    
+                    alert("Password changing success!");
+                    $("body").pagecontainer("change", "#profile", {transition: "none"});
+                  } else {                   
+                    alert("Invalid old password!");
+                  }
+                },
+                error : function(xhr, type) {
+                  alert('Server error occurred');
+                }
+              });   
+          }
+        });*/
 
--- upload.php
+++ upload.php
@@ -46,10 +46,10 @@
 	<!-- ****************************************** Zenefeltöltő form ********************************************* -->
 
 	<div id="uploadHolderDiv">
-		<div>
+		<div id="categorySelectDiv">
 			<!-- ************************************** Kategória kiválasztása ************************************ -->
-			<h3>Upload</h3>
-			<p>1. step: Choosing a category</p>
+			<h3 id="uploadTitle">Upload</h3>
+			<p class="centerText stepsText">1. step: Choosing a category</p>
 			<label for="categorySelect">In which category do you want to upload</label>
 			<select name="categorySelect" id="categorySelect">
 				<option value="Default" >Choose!</option>
@@ -66,59 +66,82 @@
 		
 				<!-- ************************************** Torrent nevének megadása ************************************ -->   
 			    <div class="">
-				    <p>2. step: Torrent nevének megadása</p>
-					<label for="torrentName">Torrent name:</label>
-					<input type="text" name="torrentName" value="" maxlength="100" size="30">
+				    <p class="centerText stepsText">2. step: Torrent nevének megadása</p>
+					<label for="torrentName">
+						<span class="uploadTextBlock">Torrent name:</span>
+						<input class="torrentName" type="text" name="torrentName" value="" maxlength="100" size="30"><span class="torrentNameError"></span>
+					</label>
 			    </div>
 
 			 	<!-- ************************************** Torrentfájl feltöltése ************************************ -->   
 			 	<div>
-			 		<p>3. step: Torrent file upload</p>
-					<input type="file" name="torrentFile"  maxlength="100000">  
+			 		<p class="centerText stepsText">3. step: Torrent file upload</p>
+					<input type="file" name="torrentFile"  maxlength="100000"><span id="torrentFileError"></span>
 				</div>
 
 				<!-- ************************************** Leírás megadása ******************************************** -->
 				<div>
-				 	<p>4. step: Describe</p>
-				 	<label for="coverImage">Upload the cover image</label>
-				 	<input type="file" name="coverImage"  maxlength="100000">
+				 	<p class="centerText stepsText">4. step: Describe</p>
+				 	<label for="coverImage">
+				 		<span class="uploadTextBlock">Upload the cover image</span>
+				 		<input type="file" name="coverImage"  maxlength="100000"><span id="coverImageError"></span>
+				 	</label>
+				 	
 
 				 	<br>
-				 	<label for="artist">Artist:</label>
-					<input type="text" name="artist" value="" maxlength="100" size="30">
+				 	<label for="artist">
+				 		<span class="uploadTextBlock">Artist:</span>
+				 		<input type="text" name="artist" value="" maxlength="100" size="30"><span id="artistError"></span>
+				 	</label>
+					
 				    
 				    <br>
-				 	<label for="album">Album:</label>
-					<input type="text" name="album" value="" maxlength="100" size="30">
+				 	<label for="album">
+				 		<span class="uploadTextBlock">Album:</span>
+				 		<input type="text" name="album" value="" maxlength="100" size="30"><span id="albumError"></span>
+				 	</label>
+					
 
 				    <br>
-				 	<label for="style">Style:</label>
-					<input type="text" name="style" value="" maxlength="100" size="30">
+				 	<label for="style">
+				 		<span class="uploadTextBlock">Style:</span>
+				 		<input type="text" name="style" value="" maxlength="100" size="30"><span id="styleError"></span>
+				 	</label>
+					
 				    
 				    <br>
-				 	<label for="format">Format:</label>
-					<input type="text" name="format" value="" maxlength="100" size="30">
+				 	<label for="format">
+				 		<span class="uploadTextBlock">Format:</span>
+				 		<input type="text" name="format" value="" maxlength="100" size="30"><span id="formatError"></span>
+				 	</label>
+					
 
 					<br>
-				 	<label for="bitrate">Bitrate:</label>
-					<input type="text" name="bitrate" value="" maxlength="100" size="30">
+				 	<label for="bitrate">
+				 		<span class="uploadTextBlock">Bitrate:</span>
+				 		<input type="text" name="bitrate" value="" maxlength="100" size="30"><span id="bitrateError"></span>
+				 	</label>
+					
 
 				    <br>
-				    <label for="description" >Description:</label>
-					<textarea class="szoveg" cols="30" rows="10" name="description"></textarea>
+				    <label for="description" >
+				    	<span class="uploadTextBlock">Description:</span>
+				    	<textarea id="descriptionTextarea" class="szoveg" cols="30" rows="10" name="description"></textarea><span id="descriptionError"></span>
+				    </label>
+					
 						
 				</div>
 
 				<!-- ************************************ Feltételek elfogadása *************************************** -->
-				<p>5. step: Feltételek elfogadása</p>
-			    <label for="conditions">Elfogadom az oldal használatának feltételeit:</label>
-			    <input type="checkbox" name="conditions" value="1">
+				<p class="centerText stepsText">5. step: Feltételek elfogadása</p>
+			    <label for="conditions"><input class="termsBox" type="checkbox" name="conditions" value="1"><span >Elfogadom az oldal használatának feltételeit</span><span id="conditionsError"></span></label>
+			    
 
 			    <!-- ****************************************** Gombok ********************************************* -->
 			    <br>
 			    <br>
-			    <input class="" type="reset" name=reset value="Clear">
-			    <input class="" type="submit" name=go value="Send">
+			    <input class="button" type="reset" name=reset value="Clear">
+			    <input id="submitButton" class="button" type="submit" name=go value="Send">
     		</form>
     	</div>
 
@@ -126,20 +149,20 @@
 	    	<form id="movieUploadForm" name="movieUploadForm" action="action.php" method="POST" enctype="multipart/form-data">
 	    			<div class="">
 	    				
-	    				<h3>2. step: Torrent nevének megadása</h3>
+	    				<p class="centerText stepsText">2. step: Torrent nevének megadása</p>
 	    				<table>
 	    					<tr>
 	    						<th><label for="torrentName">Torrent name:</label></th>
-	    						<td><input type="text" name="torrentName" value="" maxlength="100" size="30"></td>
+	    						<td><input class="torrentName" type="text" name="torrentName" value="" maxlength="100" size="30"></td><span class="torrentNameError"></span>
 	    					</tr>
 	    				</table>
-	    				<h3>3. step: Torrent file upload</h3>
+	    				<p class="centerText stepsText">3. step: Torrent file upload</p>
 	    				<table>
 	    					<tr>
 	    						<td><input type="file" name="torrentFile"  maxlength="100000"></td>
 	    					</tr>
 	    				</table>
-	    				<h3>4. step: Describe</h3>
+	    				<p class="centerText stepsText">4. step: Describe</p>
 	    				<table>
 	    					<tr>
 	    						<th><label for="coverImage">Upload the cover image</label></th>
